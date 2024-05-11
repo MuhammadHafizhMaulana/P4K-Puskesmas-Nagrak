5ecd8b83bb166bd13cedca4cdb4dae6bad1161d4
miftaSI21 <miftakhurokhmann@gmail.com>
2024-05-11 11:27:44 +0700

    update view donor darah

--- donor_darah.php
+++ donor_darah.php
@@ -12,45 +12,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/donorDarah.css">
 </head>
 
 <body>
-    <div class="container-fluid d-flex flex-column justify-content-center align-items-center">
-        <main class="row mt-5 justify-content-center align-items-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Donor Darah</h5>
-                    </div>
-                    <div class="card-body">
-                        <form action="proses/input_gol_darah_proses.php" method="post" >
-                            <div class="mb-3">
-                                <label for="nama" class="form-label">Masukkan Golongan Darah Anda</label><br>
-                                <input type="radio" id="golongan_a" name="goldar" value="a">
-                                <label for="golongan_a">Golongan A</label><br>
-
-                                <input type="radio" id="golongan_b" name="goldar" value="b">
-                                <label for="golongan_b">Golongan B</label><br>
-
-                                <input type="radio" id="golongan_ab" name="goldar" value="ab">
-                                <label for="golongan_ab">Golongan AB</label><br>
-
-                                <input type="radio" id="golongan_o" name="goldar" value="o">
-                                <label for="golongan_o">Golongan O</label><br>
-                                
-
-                                <div class="form-group">
-                                <label for="usiakandungan"> Berapa Usia Kandungan Anda (Dalam Minggu)</label>
-                                <input type="number" class="form-control" id="usiakandungan" name="usia_kandungan" placeholder="Masukan usia usia_kandungan anda" required>
-
-                                <button type="submit" class="btn btn-primary">INPUT</button>
-                                </div>
-
-                                
-                        </form>
-                        <br>
-                        <?php 
+<nav class="my-navbar navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="home.php">
+        <img src="./assets/logo-kemenkes.png" alt="Logo Kemenkes">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+          </svg></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+          <a class="nav-link" href="dashboard_donor_darah.php">Donor Darah</a>
+          <a class="nav-link" href="donor_darah.php">DonorDarahTambah</a>
+          <a class="nav-link" href="profile.php">Profile</a>
+          <a class="nav-link" href="proses/logout.php">Logout</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div id="formDonorDarah">
+        <h1 style="font-weight: bold; font-size: xxx-large">Daftarkan Golongan Darah</h1>
+        <br />
+        <?php 
                             include 'proses/koneksi.php';
                             $id = $_SESSION['id'];
                             $query = "SELECT `goldar`, `usia_kandungan` FROM `kesehatan_user` WHERE `id_user` = '$id'";
@@ -70,26 +60,32 @@
                                 echo "<div class='alert alert-danger'> Data belum diinputkan.</div>";
                             }  
                         ?>
-                        <br>
-                        <?php
-                            //pesan jika terjadi kesalahan
-                            if (isset($_GET['pesan'])) {
-                                if ($_GET['pesan'] == "gagal") {
-                                    echo "<div class='alert alert-danger'> Login gagal. Username atau password salah.</div>";
-                            }
-                            }
-                            // pesan jika sukses
-                            if (isset($_GET['success']) && $_GET['success'] == 1) {
-                                echo "<div class='alert alert-success'>Data berhasil ditambahkan!</div>";
-                            }
-                        ?>
-                        <h2><a href="home.php">Kembali ke Home</a></h2>
+        <p>
+            Masuk informasi golongan darah anda:
+        </p>
+        <form action="proses/input_gol_darah_proses.php" method="post" >
 
-                    </div>
-                </div>
+        <div class="form-group">
+        <label >Pilih golongan darah anda</label>
+        <select class="form-select" aria-label="Default select example">
+            <option value="-" selected>Belum Mengetahui</option>
+            <option value="a">A</option>
+            <option value="o">O</option>
+            <option value="b">B</option>
+        </select>
+        </div>
+
+            <div class="form-group">
+                <label for="usiakandungan"> Berapa Usia Kandungan Anda (Dalam Minggu)</label>
+                <input type="number" class="form-control" id="usiakandungan" name="usia_kandungan" placeholder="Masukan usia usia_kandungan anda" required>
             </div>
-        </main>
+
+            <br />
+
+            <button type="submit" class="btn btn-danger">INPUT</button>
+        </form>
     </div>
+
 </body>
 
 </html>
