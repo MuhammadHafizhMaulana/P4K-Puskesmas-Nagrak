3cc1cd9a42114a830332878d8ed392ad5ada4f95
miftaSI21 <miftakhurokhmann@gmail.com>
2024-05-14 21:01:48 +0700

    Membuat view daftar user di admin

--- proses/logout.php
+++ proses/logout.php
@@ -1,14 +1,13 @@
 <?php
-    //Matikan SESSION
-    session_start();
-    $_SESSION = []; 
-    session_unset();
-    session_destroy();
+//Matikan SESSION
+session_start();
+$_SESSION = [];
+session_unset();
+session_destroy();
 
-    //Matikan Cookie
-    setcookie('yudi', '', time() - 3600, "/");
-    setcookie('key', '', time() - 3600, "/");
+//Matikan Cookie
+setcookie('yudi', '', time() - 3600, "/");
+setcookie('key', '', time() - 3600, "/");
 
-    header('Location: ../index.php'); // Perhatikan tidak ada spasi setelah 'Location'
-    exit();
-?>
+header('Location: ../index.php'); // Perhatikan tidak ada spasi setelah 'Location'
+exit();
